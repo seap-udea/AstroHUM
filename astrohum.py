5f5d73c1220dc6560cb7cc17425afbcff987d8d9
Jorge Zuluaga <jorge.zuluaga@udea.edu.co>
2018-10-13 00:43:17 -0500

    Commit

--- astrohum.py
+++ astrohum.py
@@ -9,8 +9,7 @@ from google.colab import drive,files
 #Comandos mágicos de Colaboratory
 from IPython.core.magic import (register_line_magic, register_cell_magic,
                                 register_line_cell_magic)
-
-@register_upload
+@register_line_magic
 def upload(line):
     files.upload()
     
