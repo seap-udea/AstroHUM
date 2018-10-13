0a0c35ccc361330e7270dda881b34e6659857733
Jorge Zuluaga <jorge.zuluaga@udea.edu.co>
2018-10-13 00:39:44 -0500

    Commit

--- astrohum.py
+++ astrohum.py
@@ -1,2 +1,17 @@
-import numpy as np
+#########################################
+#Aquí esta la ropa limpia
+#########################################
+from google.colab import drive,files
 
+#########################################
+#Aquí esta la ropa sucia
+#########################################
+#Comandos mágicos de Colaboratory
+from IPython.core.magic import (register_line_magic, register_cell_magic,
+                                register_line_cell_magic)
+
+@register_line_magic
+def upload(line):
+    files.upload()
+    
+del upload
