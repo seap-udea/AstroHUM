055a693b57c4a54c9707043a74700878c06804b4
Jorge Zuluaga <jorge.zuluaga@udea.edu.co>
2018-10-13 04:23:18 -0500

    Commit

--- astrohum2.py
+++ astrohum2.py
@@ -9,21 +9,6 @@ import numpy as np
 #AQUÍ ESTA LA ROPA SUCIA
 ##########################################################
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-#COMANDOS MAGICOS DE IPYTHON
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-from IPython.core.magic import (register_line_magic, register_cell_magic,
-                                register_line_cell_magic)
-@register_line_magic
-def upload(line):
-    files.upload()
-del upload
-
-@register_line_magic
-def mount(line):
-    drive.mount('/content/gdrive')
-del mount
-
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 #RUTINAS NUMERICAS
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 def matriz(a):
