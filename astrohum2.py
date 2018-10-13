4dee7357f9283232c655f2ea8964421af234bcad
Jorge Zuluaga <jorge.zuluaga@udea.edu.co>
2018-10-13 04:16:38 -0500

    Commit

--- astrohum2.py
+++ astrohum2.py
@@ -4,3 +4,17 @@
 from google.colab import drive,files
 from astroquery.vizier import Vizier
 import numpy as np
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#RUTINAS
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+def fuentes(img,fwhm=3.0,umbral=5.0):
+    """
+    Extrae las fuentes de una imagen
+    """
+    from astropy.stats import sigma_clipped_stats
+    from photutils import DAOStarFinder
+    mean,median,std=sigma_clipped_stats(img,sigma=3.0,iters=5)
+    daofind=DAOStarFinder(fwhm=fwhm,threshold=umbral*std)
+    sources=daofind(img-median)
+    return sources
