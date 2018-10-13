3dfc62c431b7ba7df47e49542c3e1aa77e6cefb7
Jorge Zuluaga <jorge.zuluaga@udea.edu.co>
2018-10-13 04:19:35 -0500

    Commit

--- astrohum2.py
+++ astrohum2.py
@@ -22,3 +22,46 @@ del upload
 def mount(line):
     drive.mount('/content/gdrive')
 del mount
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#RUTINAS NUMERICAS
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+def matriz(a):
+    """
+    Convierte un arreglo estructurado en un arreglo plano
+    """
+    import numpy as np
+    a=np.array(a)
+    dt = a.dtype
+    dt = dt.descr
+    nt=[]
+    for t in dt:
+        t = (t[0], 'float64')
+        nt+=[t]
+    dt = np.dtype(nt)
+    a = a.astype(dt)
+    a=a.view(np.float).reshape(a.shape + (-1,))
+    return a
+
+def sex2dec(s):
+    """
+    Convierte un ángulo expresado en sexagesimal a su expresión en decimal.
+
+    Ejemplo: sex2dec("23 03 45") devuelve 23.0625
+    """
+    d=np.sign(float(s.split()[0]))*(np.array([np.abs(float(x)) for x in s.split()]).dot([1.0,1/60.0,1/3600.]).sum())
+    return d
+
+def dec2sex(d,fmt="list",sep=" "):
+    s=np.sign(d)
+    d=np.abs(d)
+    dg=int(d)
+    mm=(d-dg)*60
+    mg=int(mm)
+    sg=(mm-mg)*60
+    sex=None
+    if fmt=="list":
+        sex=s*dg,mg,sg
+    if fmt=="string":
+        sex="%02d%s%02d%s%02.2f"%(s*dg,sep,mg,sep,sg)
+    return sex
